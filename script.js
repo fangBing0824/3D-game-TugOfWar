691a11ad1806e8a35dc67e17a4a53bc5ff67ce75
fangBing0824 <fangbingl3@nycstudents.net>
2026-03-13 00:33:34 +0000

    updates

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ window.addEventListener('DOMContentLoaded', function() {
 
     let t = 60;
     let timeDisplay = document.getElementById('time-display');
+    let numberDisplay = document.getElementById('number-display');
+
+    let rope = this.document.getElementById('rope');
+    let camera = document.querySelector('a-camera');
+
+    let ropePosition = -12; 
+    let cameraPositionZ = 0; 
+
     let gameOver = false;
-    let timeoutId; // declare this
+    let timeoutId; 
+
+    let currentNumber = null;
+
 
     function endGame(message) {
         gameOver = true;
         alert(message);
     }
 
-    // Timer function
+// Timer function
     function countdown() {
         if (gameOver) return;
 
@@ -29,6 +40,58 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     countdown();
+// random numbers
+    function number(){
+        if (gameOver) return;
 
+        currentNumber = Math.floor(Math.random() * 10) + 1;
+
+        numberDisplay.textContent = "Press: " + currentNumber;
+    }
+
+    number();
+
+     document.addEventListener("keydown", function (event) {
+        if (gameOver) return;
+
+        let playerInput = parseInt(event.key);
+
+        if (playerInput === currentNumber) {
+           
+            ropePosition += 2;  
+            cameraPositionZ += 0.3; 
+        } else {
+           
+            ropePosition -=2;
+            cameraPositionZ -= 0.3;
+        }
+
+       
+        rope.setAttribute("position", `0 1.6 ${ropePosition}`);
+        camera.setAttribute("position", `0 1.6 ${cameraPositionZ}`);
+
+        if (cameraPositionZ <= -17) endGame("YOU LOSE!");
+        if (ropePosition >= 0) endGame("YOU WIN!");
+       
+        number();
+    });
+
+     function autoPull() {
+        if (gameOver) return;
+
+        ropePosition -= 1.0; 
+        cameraPositionZ -= 1.0;
+
+        rope.setAttribute("position", `0 1.6 ${ropePosition}`);
+        camera.setAttribute("position", `0 1.6 ${cameraPositionZ}`);
+
+        if (cameraPositionZ <= -17) endGame("YOU LOSE!");
+        if (ropePosition >= -5) endGame("YOU WIN!");
+       
+       
+    }
+
+    setInterval(autoPull, 2000); // 每2秒拉一次
     
+
 });
